dcf376e800d7e95911d8a68e645f460609b974d5
Citrom333 <citrom333@gmail.com>
2023-03-25 11:04:33 +0100

    Implement interface for room repository

--- src/DAL/InMemoryRoomRepository.cs
+++ src/DAL/InMemoryRoomRepository.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using HogwartsHouses.Models;
 
 namespace HogwartsHouses.DAL
@@ -7,7 +8,7 @@ namespace HogwartsHouses.DAL
     public class InMemoryRoomRepository : IRepository<Room>
     {
         private HashSet<Room> _rooms;
-
+        private IRepository<Room> _repositoryImplementation;
         public InMemoryRoomRepository()
         {
             SeedRooms();
@@ -16,7 +17,46 @@ namespace HogwartsHouses.DAL
         private void SeedRooms()
         {
             _rooms = new HashSet<Room>();
-            throw new NotImplementedException();
+        }
+
+        public HashSet<Room> All()
+        {
+            return _rooms;
+        }
+
+        public void Add(Room toAdd)
+        {
+            var id = _rooms.Count < 1 ? 1 : _rooms.ToList().MaxBy(r => r.Id)!.Id + 1;
+            _rooms.Add(new Room(id, toAdd.NumberOfBeds, toAdd.RoomForBoys));
+        }
+
+        public void Remove(Room toRemove)
+        {
+            _rooms.RemoveWhere(room => room.Id == toRemove.Id);
+        }
+
+        public void Update(Room toUpdate)
+        {
+            foreach (var room in _rooms)
+            {
+                if (room.Id == toUpdate.Id)
+                {
+                    room.NumberOfBeds = toUpdate.NumberOfBeds;
+                    room.RoomForBoys = toUpdate.RoomForBoys;
+                }
+            }
+        }
+
+        public bool AddRoomToStudent(Room room, Student student)
+        {
+            foreach (var r in _rooms)
+            {
+                if (r.Id == room.Id)
+                {
+                   r.OccupiedBeds.Add(student);
+                }
+            }
+            return true;
         }
     }
 }
