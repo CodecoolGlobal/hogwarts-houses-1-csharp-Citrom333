70c1dcff8798c516ff8d8f19a2f4f4e0b394b047
Citrom333 <citrom333@gmail.com>
2023-03-25 11:06:56 +0100

    Add fields and constructor

--- src/Models/Student.cs
+++ src/Models/Student.cs
@@ -1,7 +1,23 @@
+using HogwartsHouses.Models.Types;
+
 namespace HogwartsHouses.Models
 {
     [System.Serializable]
     public class Student
     {
+        public int StudentId { get; }
+        public string Name { get; }
+        public Gender Gender { get; }
+        public PetType Pet { get; set; }
+        public HouseType House { get; }
+
+        public Student(int studentId, string name, Gender gender, PetType pet, HouseType house)
+        {
+            StudentId = studentId;
+            Name = name;
+            Gender = gender;
+            Pet = pet;
+            House = house;
+        }
     }
 }
