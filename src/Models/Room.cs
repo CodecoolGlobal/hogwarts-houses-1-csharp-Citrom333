e98b3f1db22e2c39da1f78dfc5fa58b3952dc337
Citrom333 <citrom333@gmail.com>
2023-03-25 11:06:42 +0100

    Add fields and constructor

--- src/Models/Room.cs
+++ src/Models/Room.cs
@@ -1,7 +1,24 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.StaticFiles;
+
 namespace HogwartsHouses.Models
 {
     [System.Serializable]
     public class Room
     {
+        public int Id { get; }
+        public int NumberOfBeds { get; set; }
+        public List<Student> OccupiedBeds { get; set; }
+        public bool RoomForBoys { get; set; }
+
+        public Room(int id, int numberOfBeds, bool boyRoom)
+        {
+            Id = id;
+            NumberOfBeds = numberOfBeds;
+            RoomForBoys = boyRoom;
+            OccupiedBeds = new List<Student>(NumberOfBeds);
+           
+        }
     }
 }
