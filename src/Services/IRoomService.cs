055ec8da21ff4d52827767fb05002a701f57d8a1
Citrom333 <citrom333@gmail.com>
2023-03-25 11:05:30 +0100

    Add methods for room service interface

--- src/Services/IRoomService.cs
+++ src/Services/IRoomService.cs
@@ -1,6 +1,17 @@
+using System;
+using System.Collections.Generic;
+using HogwartsHouses.Models;
+
 namespace HogwartsHouses.Services
 {
     public interface IRoomService
     {
+        public List<Room> GetAllRooms();
+        public void AddRoom(Tuple<int, bool> roomDetails);
+        public void DeleteRoom(int id);
+        public void UpdateRoom(int id, Tuple<int, bool> roomDetails);
+        public void PutStudentToRoom(int roomId, int studentId);
+        public List<Room> GetRoomsWithFreeBed();
+        public List<Room> GetRoomsForRatKeepers();
     }
 }
