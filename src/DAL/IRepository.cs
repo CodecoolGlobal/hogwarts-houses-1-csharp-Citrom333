6582b83b7018bd08ed74c63a3ba21a149a10039f
Citrom333 <citrom333@gmail.com>
2023-03-25 11:03:54 +0100

    Add methods for repository interface

--- src/DAL/IRepository.cs
+++ src/DAL/IRepository.cs
@@ -1,6 +1,14 @@
+using System.Collections.Generic;
+using HogwartsHouses.Models;
+
 namespace HogwartsHouses.DAL
 {
     public interface IRepository<T>
     {
+        public HashSet<T> All();
+        public void Add(T toAdd);
+        public void Remove(T toRemove);
+        public void Update(T toUpdate);
+        public bool AddRoomToStudent(Room room, Student student);
     }
 }
