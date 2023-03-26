859a8abfc2acda5dc5e802a8ea19e1476797a123
Citrom333 <citrom333@gmail.com>
2023-03-26 22:48:09 +0200

    Add constructor overload and ToString override

--- src/Models/Student.cs
+++ src/Models/Student.cs
@@ -5,11 +5,11 @@ namespace HogwartsHouses.Models
     [System.Serializable]
     public class Student
     {
-        public int StudentId { get; }
-        public string Name { get; }
-        public Gender Gender { get; }
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public Gender Gender { get; set; }
         public PetType Pet { get; set; }
-        public HouseType House { get; }
+        public HouseType House { get; set; }
 
         public Student(int studentId, string name, Gender gender, PetType pet, HouseType house)
         {
@@ -18,6 +18,17 @@ namespace HogwartsHouses.Models
             Gender = gender;
             Pet = pet;
             House = house;
+        }
+
+        public Student()
+        {
+            
+        }
+        public override string ToString()
+        {
+            return
+                $"Student ID: {StudentId}, name: {Name}. Gender: {Gender}. Type of pet: {Pet}. " +
+                $"The student is in {House} ";
         }
     }
 }
