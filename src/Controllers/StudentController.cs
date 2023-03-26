b7b5053cf400432c347f46d39f2eff705f19b0ce
Citrom333 <citrom333@gmail.com>
2023-03-26 22:48:32 +0200

    Add Views for requests

--- src/Controllers/StudentController.cs
+++ src/Controllers/StudentController.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using HogwartsHouses.Models;
+using HogwartsHouses.Models.Types;
 using HogwartsHouses.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
 namespace HogwartsHouses.Controllers;
-[ApiController, Route("/students")]
+
+// [ApiController, 
+[Route("/students")]
 public class StudentController : Controller
 {
     private IStudentService _studentService;
@@ -15,31 +20,95 @@ public class StudentController : Controller
     {
         _studentService = studentService;
     }
+
+    private String MakeTheListToString(List<Student> students)
+    {
+        string stString = "";
+        foreach (var student in students)
+        {
+            stString += $"</br>{student.ToString()}";
+        }
+
+        return stString;
+    }
+
     [HttpGet("AllStudents")]
-    public List<Student> GetAll()
+    public IActionResult GetAll()
     {
-        return _studentService.GetAllStudents();
+        ViewData["All"] = MakeTheListToString(_studentService.GetAllStudents());
+        return View();
     }
-    [HttpGet("Student/{id}")]
-    public Student GetStudents(int id)
+
+    [HttpGet("Student")]
+    public IActionResult GetStudents()
     {
-        return _studentService.GetAllStudents().FirstOrDefault(student => student.StudentId==id);
+        return View("FindStudent");
     }
-    
-    [HttpPost("NewStudent")]
-    public void AddNewStudent([FromBody] Student student)
+
+    [HttpPost("Student")]
+    public IActionResult GetStudents(Student student)
     {
-        Console.WriteLine($"New Student {student.Name}, in {student.House}");
+        int id = student.StudentId;
+        if (_studentService.GetAllStudents().Select(s => s.StudentId).Contains(id))
+        {
+            Console.WriteLine(id);
+            ViewData["result"] = _studentService.GetAllStudents().FirstOrDefault(student => student.StudentId == id)
+                .ToString();
+            return View("Success");
+        }
+        else
+        {
+            return View("WrongFillOut");
+        }
+    }
+
+    [HttpGet("NewStudent")]
+    public IActionResult AddNewStudent()
+    {
+        return View();
+    }
+
+    [HttpPost("NewStudent/{overload}")]
+    public IActionResult AddNewStudent([FromBody] Student student, bool overload)
+    {
         _studentService.AddStudent(student);
+        ViewData["result"] = $"New Student {student.Name}, in {student.House}";
+        return View("Success");
     }
+
+    [HttpPost("NewStudent")]
+    public IActionResult AddNewStudent(Student student)
+    {
+        string name = student.Name;
+        Gender gender = student.Gender;
+        PetType pet = student.Pet;
+        HouseType house = student.House;
+        if (name == null || gender == 0 || pet == 0 || house == 0)
+        {
+            return View("WrongFillOut");
+        }
+        else
+        {
+            _studentService.AddStudent(new Student(default, name, gender, pet, house));
+            ViewData["house"] = house;
+            ViewData["name"] = name;
+            return View("SortingHat");
+        }
+    }
+
     [HttpDelete("DeleteStudent/{id}")]
-    public void DeleteStudent(int id)
+    public IActionResult DeleteStudent(int id)
     {
         _studentService.DeleteStudent(id);
+        return Content($"New Student {id} deleted.");
     }
-    [HttpDelete("UpdateStudent/{id}")]
-    public void UpdateStudent(int id, [FromBody] Student studentDetails)
+
+    [HttpPut("UpdateStudent/{id}")]
+    public IActionResult UpdateStudent(int id, [FromBody] Student studentDetails)
     {
-        _studentService.UpdateStudent(new Student(id, studentDetails.Name, studentDetails.Gender,studentDetails.Pet, studentDetails.House));
+        _studentService.UpdateStudent(new Student(id, studentDetails.Name, studentDetails.Gender, studentDetails.Pet,
+            studentDetails.House));
+        ViewData["result"] = $"New Student {studentDetails.Name} updated.";
+        return View("Success");
     }
 }
