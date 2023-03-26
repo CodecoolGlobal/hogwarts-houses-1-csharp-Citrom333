3905aeb519e2d66da00c2331fec81dadd34525a4
Citrom333 <citrom333@gmail.com>
2023-03-26 22:36:49 +0200

    Add View for Greeting as Index page

--- src/Controllers/GreetingController.cs
+++ src/Controllers/GreetingController.cs
@@ -2,13 +2,18 @@
 
 namespace HogwartsHouses.Controllers
 {
-    [ApiController, Route("/")]
-    public class GreetingController : ControllerBase
+    // [ApiController, Route("/")]
+    public class GreetingController : Controller
     {
-        [HttpGet("{name?}")]
         public string Greeting(string name = "Witches and Wizards")
         {
             return $"Welcome to Hogwarts, {name}";
+        }
+        [HttpGet("{name?}")]
+        public IActionResult Index(string name = "Witches and Wizards")
+        {
+            ViewData["Greeting"] = Greeting(name);
+            return View();
         }
     }
 }
