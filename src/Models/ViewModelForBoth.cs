0e41b5c1148f3ad5b5f70bc08ad65588c2cd55a6
Citrom333 <citrom333@gmail.com>
2023-03-26 22:52:39 +0200

    Add Common model (room and student) to handle in one view both

---
+++ src/Models/ViewModelForBoth.cs
@@ -0,0 +1,13 @@
+namespace HogwartsHouses.Models;
+
+public class ViewModelForBoth
+{
+    public Room Room { get; set; }
+
+    public Student Student { get; set; }
+
+    public ViewModelForBoth()
+    {
+        
+    }
+}
